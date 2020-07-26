cf4448228477d2d88cd93a643f763a9807f2882b
Daniel Loomer <fuzetsu@users.noreply.github.com>
2020-07-25 22:37:13 -0400

    1.0.6 check for state changes on subscribe - since useLayoutEffect is async state changes between useStore running and the sub getting added were not noticed by staterino - refactor shaved off -7 bytes on the gzip dist - upgraded deps

--- staterino.js
+++ staterino.js
@@ -4,15 +4,25 @@ const runCallback = (callback, slice, isArr) => callback[isArr ? 'apply' : 'call
 
 const SELF = x => x
 
-const createStore = ({ merge, hooks: { useReducer, useLayoutEffect }, state = {} }) => {
-  // track subs using set
-  const subs = new Set()
-  const subscribe = sub => (subs.add(sub), () => subs.delete(sub))
-
+const staterino = ({ merge, hooks: { useReducer, useLayoutEffect }, state = {} }) => {
   // evaluate selectors
   const runSelectorPart = part => (typeof part === 'string' ? getPath(state, part) : part(state))
   const runSelector = sel =>
     Array.isArray(sel) ? [sel.map(runSelectorPart), true] : [runSelectorPart(sel), false]
+
+  const checkSub = sub => {
+    const [slice, isArr] = runSelector(sub._selector)
+    if (isArr ? !arrEqual(slice, sub._slice) : slice !== sub._slice)
+      runCallback(sub._callback, (sub._slice = slice), isArr)
+  }
+
+  // track subs using set
+  const subs = new Set()
+  const subscribe = sub => {
+    subs.add(sub)
+    checkSub(sub)
+    return () => subs.delete(sub)
+  }
 
   // main hook, manages subscription and returns slice
   const useStore = (selector = SELF) => {
@@ -34,21 +44,14 @@ const createStore = ({ merge, hooks: { useReducer, useLayoutEffect }, state = {}
   useStore.set = (...patches) => {
     state = merge(state, patches)
     // when state is patched check if each subs slice of state has changed, and callback if so
-    subs.forEach(sub => {
-      const [slice, isArr] = runSelector(sub._selector)
-      if (isArr ? !arrEqual(slice, sub._slice) : slice !== sub._slice)
-        runCallback(sub._callback, (sub._slice = slice), isArr)
-    })
+    subs.forEach(checkSub)
   }
 
-  // external subscription
-  useStore.subscribe = (callback, selector = SELF) => {
-    const [slice, isArr] = runSelector(selector)
-    runCallback(callback, slice, isArr)
-    return subscribe({ _callback: callback, _selector: selector, _slice: slice })
-  }
+  // external subscription, pass empty arr as slice to force callback to be called on first run
+  useStore.subscribe = (callback, selector = SELF) =>
+    subscribe({ _callback: callback, _selector: selector, _slice: [] })
 
   return useStore
 }
 
-export default createStore
+export default staterino
